840c27efdce4edb5756b96f82674503d28599fa3
xatomen <jorgito899@gmail.com>
2023-06-02 23:44:17 -0400

    Actualización 23:44 02-06-2023

--- index.php
+++ index.php
@@ -1,37 +1,46 @@
 <?php include('template/header.php'); ?>
 
+<?php
+
+include("./admin/config/bd.php");
+
+$sentenciaSQL= $conexion->prepare("SELECT * FROM IMAGEN_CARRUSEL");
+$sentenciaSQL->execute();
+$listaImagenes=$sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
+
+
 <div class="box m-5">
     <h1 class="title">Inicio</h1>
     <hr>
     <p1>hola Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo id iure, quam fuga illum iusto optio neque deserunt tempora distinctio eum corrupti? Ratione fugiat maxime atque harum aliquam unde! Et. hola</p1>
         
-    <div class="box">
-
-    </div>
 
     <div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="https://www.tooltyp.com/wp-content/uploads/2014/10/1900x920-8-beneficios-de-usar-imagenes-en-nuestros-sitios-web.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-inner">
+
+        <!-- ACÁ DEBEMOS PONER EL PHP CODE -->
+        <?php foreach($listaImagenes as $link_imagen) { ?>
+            <div class= "<?php if($link_imagen['ID_IMG_CARRUSEL']==1){echo "carousel-item active";}else{echo "carousel-item";}?>">
+                <img src="<?php echo $link_imagen['IMAGEN_CARRUSEL'] ?>" class="d-block w-100" alt="...">
+            </div>
+        <?php } ?>
+
+
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
     </div>
-    <div class="carousel-item">
-      <img src="https://www.tooltyp.com/wp-content/uploads/2014/10/1900x920-8-beneficios-de-usar-imagenes-en-nuestros-sitios-web.jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="https://www.tooltyp.com/wp-content/uploads/2014/10/1900x920-8-beneficios-de-usar-imagenes-en-nuestros-sitios-web.jpg" class="d-block w-100" alt="...">
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
     
-</div>
+ </div>
 
 <script>
     const carousel = new bootstrap.Carousel('#carouselExample')
--- preguntas_frecuentes.php
+++ preguntas_frecuentes.php
@@ -1,11 +1,48 @@
 <?php include('template/header.php'); ?>
 
+<?php
+
+include("./admin/config/bd.php");
+
+$sentenciaSQL= $conexion->prepare("SELECT * FROM PREGUNTAS_FRECUENTES");
+$sentenciaSQL->execute();
+$listaPreguntas=$sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
 <div class="box m-5">
     <h1 class="title">Preguntas frecuentes</h1>
     <hr>
     <p1>hola Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo id iure, quam fuga illum iusto optio neque deserunt tempora distinctio eum corrupti? Ratione fugiat maxime atque harum aliquam unde! Et. hola</p1>
 
     <div class="accordion" id="accordionPanelsStayOpenExample">
+        <?php foreach ($listaPreguntas as $pregunta) {?>
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse<?php echo $pregunta['ID_PREGUNTA'] ?>" aria-expanded="false" aria-controls="panelsStayOpen-collapse<?php echo $pregunta['ID_PREGUNTA'] ?>">
+                <?php echo $pregunta['TITULO_PREGUNTA'] ?>
+                </button>
+                </h2>
+                <div id="panelsStayOpen-collapse<?php echo $pregunta['ID_PREGUNTA'] ?>" class="accordion-collapse collapse">
+                    <div class="accordion-body">
+                        <?php echo $pregunta['DESCRIPCION_PREGUNTA'] ?>
+                    </div>
+                </div>
+            </div>
+        <?php } ?>
+    </div>
+
+<script>
+    
+</script>
+
+
+</div>
+
+<?php include('template/footer.php'); ?>
+
+
+<!-- <div class="accordion" id="accordionPanelsStayOpenExample">
         <div class="accordion-item">
             <h2 class="accordion-header">
             <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
@@ -42,13 +79,45 @@
             </div>
             </div>
         </div>
-    </div>
-
-<script>
-    
-</script>
+    </div> -->
 
 
-</div>
 
-<?php include('template/footer.php'); ?>
+    <!-- <div class="accordion" id="accordionPanelsStayOpenExample">
+        <div class="accordion-item">
+            <h2 class="accordion-header">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse1" aria-expanded="false" aria-controls="panelsStayOpen-collapse1">
+                Accordion Item #1
+            </button>
+            </h2>
+            <div id="panelsStayOpen-collapse1" class="accordion-collapse collapse">
+                <div class="accordion-body">
+                    <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                </div>
+            </div>
+        </div>
+        <div class="accordion-item">
+            <h2 class="accordion-header">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse2" aria-expanded="false" aria-controls="panelsStayOpen-collapse2">
+                Accordion Item #2
+            </button>
+            </h2>
+            <div id="panelsStayOpen-collapse2" class="accordion-collapse collapse">
+                <div class="accordion-body">
+                    <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                </div>
+            </div>
+        </div>
+        <div class="accordion-item">
+            <h2 class="accordion-header">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse3" aria-expanded="false" aria-controls="panelsStayOpen-collapse3">
+                Accordion Item #3
+            </button>
+            </h2>
+            <div id="panelsStayOpen-collapse3" class="accordion-collapse collapse">
+                <div class="accordion-body">
+                    <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                </div>
+            </div>
+        </div>
+    </div> -->
